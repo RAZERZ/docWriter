efb939c51ff7b7bb3d5e52d0fee2945cbfe336b3
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-05-08 00:41:27 +0200

    Added initial storage schema

--- background/background.js
+++ background/background.js
@@ -1,5 +1,30 @@
 chrome.runtime.onInstalled.addListener(() => {
 
+    let staticStorage = {
+        docWriter: {
+            preferences: {
+                autoLoad: 1,
+                saveLastSession: 1
+            },
+            documents: {
+                activeDocs: {
+                    docId: [],
+                    limit: {
+                      word: [],
+                      page: []
+                    },
+                    prevSession: [],
+                    currSession: []
+                },
+                inactiveDocs: {
+                    docId: []
+                }
+            }
+        }
+    };
+
+    chrome.storage.sync.set(staticStorage);
+
     chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({pageUrl:{hostEquals:'docs.google.com'}})],
