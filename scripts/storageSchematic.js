207d669bd317b9c5f16e2669c69f5bc302c7e218
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-05-10 00:34:54 +0200

    Added schematic for chrome.storage

---
+++ scripts/storageSchematic.js
@@ -0,0 +1,25 @@
+let staticStorage = {
+    docWriter: {
+        preferences: {
+            autoLoad: 1,
+            saveLastSession: 1
+        },
+        documents: {
+            activeDocs: {
+                docId: [],
+                title: [],
+                limit: {
+                    deadline: [],
+                    word: [],
+                    page: []
+                },
+                progress: [],
+                prevSession: [],
+                currSession: []
+            },
+            inactiveDocs: {
+                docId: []
+            }
+        }
+    }
+};
