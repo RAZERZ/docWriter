ef2238a71ece0cda5bb3d5edecc0a11bce48f890
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-05-10 00:34:01 +0200

    Cleaned up unused code and added function to get current documents

--- scripts/initial.js
+++ scripts/initial.js
@@ -3,6 +3,38 @@ let navBarBtns = document.getElementById("navBar").children;
 navBarBtns[0].addEventListener("click", () => {
     for(let i=0;navBarBtns.length>i;i++){navBarBtns[i].removeAttribute("class");}
     navBarBtns[0].setAttribute("class", "active");
+
+    document.getElementById("content").innerHTML = '<div class="overview"><div class="activeDocs"></div></div>';
+
+    chrome.storage.sync.get("docWriter", resp => {
+        let docWriter = resp.docWriter;
+        let activeDocs = document.getElementsByClassName("activeDocs")[0];
+
+        for(let i = 0; i < docWriter.documents.activeDocs.docId.length; i++) {
+
+            let progressPct;
+            let docLimit;
+
+            if(docWriter.documents.activeDocs.limit.word[i] !== "null") {
+                docLimit = docWriter.documents.activeDocs.limit.word[i];
+            }
+            else if(docWriter.documents.activeDocs.limit.page[i] !== "null") {
+                docLimit = docWriter.documents.activeDocs.limit.page[i];
+            }
+            else {
+                console.log("No limit set");
+            }
+
+            progressPct = docWriter.documents.activeDocs.progress[i]/docLimit;
+
+            let bgColor = getColorForPercentage(progressPct);
+
+            activeDocs.innerHTML += '<div class="listElement"><p class="title">' + docWriter.documents.activeDocs.title[i] + '</p><div class="activeContent"><span class="deadline">' + docWriter.documents.activeDocs.limit.deadline[i] + '</span><div class="progress"><div class="progressbar" style="width:' + progressPct*100 + '%;background:' + bgColor + ';"></div></div></div></div>';
+
+        }
+
+    });
+
 });
 
 navBarBtns[1].addEventListener("click", () => {
@@ -15,26 +47,36 @@ navBarBtns[2].addEventListener("click", () => {
     navBarBtns[2].setAttribute("class", "active");
 });
 
-//TODO: Evaluate if this code block is necessary, considering content_script.js
-/*injectScript = () => {
-    updateWordCount = () => {
-        let wordCount = document.getElementsByClassName("kix-paginateddocumentplugin")[0].innerText.match(/\w+/g).length;
-        let docWriterProgressbar = document.getElementById("docWriterProgressbar");
-        let docWriterProgressbarParent = document.getElementById("docWriterProgressbarParent");
-        docWriterProgressbar.style.width = (((wordCount)/(word limit. TODO: Fetch from chrome storage 5000))*100) + "%";
-        docWriterProgressbarParent.getElementsByClassName("wc")[0].innerText = wordCount;
-        return wordCount;
-    };
 
-    if(document.getElementById("docWriterProgressbarParent") == undefined) {
-        let templateElement = document.createElement("template");
-        templateElement.innerHTML = '<div id="docWriterProgressbarParent" style="position: absolute;width:30%;height: 2.5%;top:35px;left:800px"><span class="wc" style="color:#171717;font-size:1.1em;padding:0.5% 4%;border:1px solid gray;border-radius:50px"></span><div style="width:85%;height:100%;float:right;background:#212121;border-radius:50px;z-index:999"><div id="docWriterProgressbar" style="width: 0%;height: 100%;background: #00ad00;border-radius: 50px;transition:2s ease"></div></div></div>';
-        let genEl = templateElement.content.childNodes[0];
-        document.body.insertBefore(genEl, document.body.children[0]);
+//Event listeners
+
+window.addEventListener("load", () => {
+    navBarBtns[0].click();
+});
+
+//Temp code
+
+let percentColors = [
+    { pct: 0.0, color: { r: 0xff, g: 0x00, b: 0 } },
+    { pct: 0.5, color: { r: 0xff, g: 0xff, b: 0 } },
+    { pct: 1.0, color: { r: 0x00, g: 0xff, b: 0 } } ];
+
+let getColorForPercentage = pct => {
+    for (var i = 1; i < percentColors.length - 1; i++) {
+        if (pct < percentColors[i].pct) {
+            break;
+        }
     }
-
-    document.getElementsByClassName("kix-paginateddocumentplugin")[0].addEventListener("DOMSubtreeModified", () => {updateWordCount()});
-    updateWordCount();
-};*/
-
-chrome.tabs.executeScript({code:'(' + injectScript +')();'});
+    let lower = percentColors[i - 1];
+    let upper = percentColors[i];
+    let range = upper.pct - lower.pct;
+    let rangePct = (pct - lower.pct) / range;
+    let pctLower = 1 - rangePct;
+    let pctUpper = rangePct;
+    let color = {
+        r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
+        g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
+        b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
+    };
+    return 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
+};
